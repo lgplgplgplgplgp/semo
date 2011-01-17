29078db1ab6753272b03c31077ed58901e0a2a2f
wqw85@sina.com <wqw85@sina.com@1784478a-1fc1-9724-7652-993cd84a5f14>
2011-01-17 08:43:33 +0000

    去除“arm-back-para”的寄存器分配动作

---
+++ trunk/arm-back-para/arm-asm-lexer.h
@@ -0,0 +1,90 @@
+
+
+/*
+
++	ARMv ASM lexer , part of SC arm-back-para
+
++	Semo C()mpiler is a free software created by (c)Techniques of Knowledge since 20080202.
++	(C)TOK Jelo Wang
+
++	You can redistribute it and/or modify it under the terms of the gnu general public license	
++	as published by the free software foundation, either version 3 of the license or any later 	
++	version.this program is distributed in the hope that it will be useful,but without any 		
++	warranty.without even the implied warranty of merchantability or fitness for a particular 	
++	purpose.																					
+																												
++	(c)	Techniques of Knowledge
++		an open source group since 2008
++		page : http://www.tok.cc
++		email : wqw85@sina.com
+
+*/
+
+# ifndef __ARM_ASM_LEXER
+# define __ARM_ASM_LEXER
+
+# include "sccl.h"
+
+typedef enum {
+
+	//	author : Jelo Wang
+	//	since : 20100723
+	//	(C)TOK
+
+	AASM_FEN = 0x1000,			//;
+	AASM_EQU ,					//=
+	AASM_ZKL , 					//[
+	AASM_ZKR , 				   	//]
+	AASM_DOU , 				   	//,
+	AASM_XY ,  					//'
+	AASM_DY ,  					//"
+	AASM_CHA , 				   	//char
+	AASM_STR , 				  	//string
+	AASM_MAO ,					//:
+	AASM_SHARP ,				//#	
+	AASM_ENTER ,				
+	AASM_CHROW ,
+	AASM_VAR ,
+	AASM_ARRAY ,
+	AASM_VARDEF ,
+	AASM_ARRAYDEF ,
+	AASM_PROCDEF ,
+	AASM_PROCCALL ,
+	AASM_LABEL ,
+	AASM_INTENUM ,
+	AASM_FLTNUM ,
+	AASM_FLTENUM ,
+	
+
+} ARMASM_SYMBOLS ;
+
+typedef struct {
+
+	//	author : Jelo Wang
+	//	since : 20100723
+	//	(C)TOK
+
+	char c ;
+	char* token ;
+
+	int v ;
+	int pv ;
+
+	int set ;
+	int stop ; 
+
+	SCClStack stack ;
+	SCClString* code ;
+	
+} LEXERARM ;
+
+# endif
+
+extern LEXERARM* lexarm ;
+
+extern LEXERARM* lexerarm_new ( char* asm , int set ) ;
+extern void lexerarm_set ( LEXERARM* lexerarm ) ;
+extern int lexerarm_genv () ;
+extern int lexerarm_head_genv ( int border )  ;
+extern int lexerarm_destroy () ;
+
---
+++ trunk/arm-back-para/arm-asmor.c
@@ -0,0 +1,286 @@
+
+/*
+
++	ARMv ASM generator , part of SC arm-back-para
+
++	Semo C()mpiler is a free software created by (c)Techniques of Knowledge since 20080202.
++	(C)TOK Jelo Wang
+
++	You can redistribute it and/or modify it under the terms of the gnu general public license	
++	as published by the free software foundation, either version 3 of the license or any later 	
++	version.this program is distributed in the hope that it will be useful,but without any 		
++	warranty.without even the implied warranty of merchantability or fitness for a particular 	
++	purpose.																					
+																												
++	(c)	Techniques of Knowledge
++		an open source group since 2008
++		page : http://www.tok.cc
++		email : wqw85@sina.com
+
+*/
+
+# include "sc.h"
+# include "schal.h"
+# include "sccl.h"
+# include "lac-grammar.h"
+# include "lac-lexer.h"
+# include "Regoc.h"
+# include "arm-asm.h"
+# include "arm-asmor.h"
+
+SCClString ArmAsm = { 0 , 0 , 0 , 0 , 0 } ;
+
+# define DUA_OPERATOR(x) (LAC_SUB <= x && x <= LAC_HE)
+
+static void asmor_arm_gen_exp ( SCClString* lac ) {
+
+	//	author : Jelo Wang
+	//	since : 20100723
+	//	(C)TOK	
+
+	//	generate the form like '%$R.n = An-1+An' into ARM codes
+	//	results ADD Rn , An , An-1
+
+	char Rd [32] = {0} ;
+	char Rn [32] = {0} ;
+	char Rm [32] = {0} ;
+
+	int state = 0 ;
+
+	//	backup the orignal lexer
+	int lexerac = lexerac_get () ;
+	
+	//	generate a new laxer
+	lexerac_set ( lexerac_new ( lac->data , LEXLAC_FLITER_MODE ) ) ; 
+	lexerac_ready () ;
+		
+	while ( !lexac->stop ) {
+
+		lexerac_genv () ;
+
+		switch ( state ) {
+			case 0 :
+				if ( LAC_VREG == lexac->v ) {
+					state = 1  ;
+					sc_strcpy ( Rd , lexac->token ) ;
+				}
+			break ;
+			case 1 :
+				if ( LAC_VAR == lexac->v ) {
+					state = 2  ;
+					sc_strcpy ( Rn , lexac->token ) ;
+				}
+			break ;
+			case 2 :
+				if ( DUA_OPERATOR(lexac->v) ) {
+					state = 3  ;
+				}
+			break ;
+			case 3 :
+				if ( LAC_VAR == lexac->v ) {
+					lexac->stop = 1 ;
+					sc_strcpy ( Rm , lexac->token ) ;
+					THUMB_ADD_RdRnRm ("R0","R0","R0") ;
+				}
+			break ;
+		}
+			
+	}
+
+	//	set the orignal lexer
+	lexerac_set ( lexerac ) ;
+		
+}
+
+static int asmor_arm_read_variable_inf () {
+
+	
+	//	author : Jelo Wang
+	//	since : 20100722
+	//	(C)TOK
+
+	if ( LAC_VAR != lexac->v ) return 0 ;
+	
+	return 1 ;
+
+}
+
+static int asmor_arm_read_virtual_reg () {
+
+	
+	//	author : Jelo Wang
+	//	since : 20100722
+	//	(C)TOK
+
+	SCClString lac = { 0 , 0 , 0 , 0 , 0 } ;
+
+	if ( LAC_VREG != lexac->v ) return 0 ;
+
+	SCClStringInit ( &lac ) ;
+	SCClStringAddStr ( &lac , lexac->token ) ;
+	
+	while ( !lexac->stop ) {
+
+		lexerac_genv () ;
+		
+		if ( LAC_FEN == lexac->v ) break ;
+
+		SCClStringAddStr ( &lac , lexac->token ) ;
+		
+	}
+
+	SCClStringAdd ( &lac , '\0' ) ;
+	
+	//	generate arm-exp form
+	asmor_arm_gen_exp ( &lac ) ;
+
+	return 1 ;
+
+}
+
+
+static int asmor_arm_read_symbol_inf () {
+
+	//	author : Jelo Wang
+	//	since : 20100722
+	//	(C)TOK
+
+	switch ( lexac->v ) {
+		case LAC_VAR :
+			return asmor_arm_read_variable_inf () ;
+		break ;
+		case LAC_VREG :
+			return asmor_arm_read_virtual_reg () ;
+		break ;
+		default : return 0 ;
+	}
+
+	return 1 ;
+	
+}
+
+static int asmor_arm_read_symbol_def () {
+
+	//	author : Jelo Wang
+	//	since : 20100722
+	//	(C)TOK
+
+	switch ( lexac->v ) {
+
+		default : return 0 ;
+	}
+
+	return 1 ;
+	
+}
+
+int asmor_arm_funcdef () {
+
+	//	author : Jelo Wang
+	//	since : 20100505
+	//	(C)TOK
+
+	int stack = 0 ;
+	
+	if ( LAC_FUNCDEF != lexac->v ) return 0 ;
+
+	SCClStringAddStr ( &ArmAsm , "proc " ) ;
+	SCClStringAddStr ( &ArmAsm , lexac->token ) ;
+	SCClStringAddStr ( &ArmAsm , "\r\n" ) ;
+
+	//	parsing parameter scope of function
+	stack = 0 ;
+	while ( !lexac->stop ) {
+		
+		lexerac_genv () ;
+		
+		if ( LAC_XKL == lexac->v )
+			stack ++ ;
+		if ( LAC_XKR == lexac->v )
+			stack -- ;
+		
+		if ( 0 == stack ) 
+			break ;
+		
+	}
+
+	//	parsing function body
+	stack = 0 ;
+	while ( !lexac->stop ) {
+		
+		lexerac_genv () ;
+		
+		if ( LAC_DKL == lexac->v )
+			stack ++ ;
+		if ( LAC_DKR == lexac->v )
+			stack -- ;
+		
+		if ( 0 == stack ) 
+			break ;		
+
+		if ( LAC_LABEL == lexac->v ) {
+			SCClStringAddStr ( &ArmAsm , sc_strcat ( lexac->token , ":\r\n")  ) ;
+			continue ;
+		}
+
+		asmor_arm_read_symbol_def () ;
+		asmor_arm_read_symbol_inf () ;
+		
+	}	
+	
+	SCClStringAddStr ( &ArmAsm , "end\r\n" ) ;
+	
+	
+}
+
+char* asmor_arm_run ( char* lac , char* asmout ) {
+
+	//	author : Jelo Wang
+	//	since : 20100505
+	//	(C)TOK
+	
+	char* reg[15] = {
+
+		//	Normal registers that can be programed on ARM
+		{"R1"} , {"R2"} , {"R3"} ,
+		{"R4"} , {"R5"} , {"R6"} ,
+		{"R7"} , 	
+		
+	} ;
+
+	if ( !lac ) return 0 ;
+
+	//	create reg-pool of register allocator 
+	//	only 7 registers that can be used in ARM
+//	RegocRegPoolCreate ( reg , 7 ) ;
+	//	create ref graph
+//	RegocBuildRefGraph ( 0 )  ;
+	//	set a swapper to Regoc
+//	RegocSetSwapper ( &ArmAsm ) ;
+	
+	lexerac_set ( lexerac_new ( lac , LEXLAC_FLITER_MODE ) ) ; 
+	lexerac_ready () ;
+
+	SCClStringInit ( &ArmAsm ) ;
+	
+	for ( lexerac_genv () ; !lexac->stop ; lexerac_genv () ) {
+		if ( LAC_FUNCDEF == lexac->v ) {
+			asmor_arm_funcdef () ;
+		} 
+	}
+
+	SCClStringAdd ( &ArmAsm , '\0' ) ;
+
+	if ( SC_SASM & compiler->parameter ) {
+		int file = SCHalFileOpen ( asmout , "wb+" ) ;
+		SCHalFileWrite ( file , ArmAsm.data , 1 , sc_strlen (ArmAsm.data) ) ;
+		SCHalFileClose ( file ) ;
+		SCFree ( asmout ) ;
+	}	
+	
+	RegocDestroy () ;
+	
+	return ArmAsm.data ;
+
+}
+
